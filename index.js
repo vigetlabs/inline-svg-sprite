efb80fede76beed58b067211b08481e1d7519bab
Dan Tello <dan.tello@viget.com>
2016-05-04 20:48:08 -0400

    Generate id and prevent inlining duplicate sprites

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var addSvg      = require('./lib/addSvg')
 var updateHrefs = require('./lib/updateHrefs')
+var generateId  = require('./lib/generateId')
 
 module.exports = function inlineSvgSprite(iconPath, iconSelector) {
   var xhr = new XMLHttpRequest()
@@ -7,7 +8,7 @@ module.exports = function inlineSvgSprite(iconPath, iconSelector) {
   xhr.send()
   xhr.onload = function() {
     if(xhr.status === 200) {
-      addSvg(xhr.responseText)
+      addSvg(xhr.responseText, generateId(iconPath))
       updateHrefs(iconSelector || 'svg.icon')
     }
   }
